6acf57c165b0d0cde44245cda210ec56a54fce4d
HariharNangre <you@example.com>
2025-03-20 02:57:35 +0530

    Add new Files

--- Backend/index.js
+++ Backend/index.js
@@ -126,6 +126,6 @@ app.get("/:shortUrl" , async(req , res) => {
 
 
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000 , ()=>console.log("Server started on port 3000"));
+app.listen(PORT , ()=>console.log("Server started on port 3000"));
